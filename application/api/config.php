83f81cd950c9e99bb7fdf797005eb3317d062ffe
h <472131863@qq.com>
2023-09-14 17:39:18 +0800

    .

---
+++ application/api/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//配置文件
+return [
+    'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
+    'default_return_type'    => 'json',
+    'image_url' => "https://api.risecrowd.org",
+    'invite_url' => "https://www.risecrowd.org"
+];
