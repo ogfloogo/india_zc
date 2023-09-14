aeefa4b5b5e3ce49d7916e8562682544f785b395
h <472131863@qq.com>
2023-09-14 17:55:28 +0800

    1

--- application/api/config.php
+++ application/api/config.php
@@ -4,6 +4,6 @@
 return [
     'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
     'default_return_type'    => 'json',
-    'image_url' => "https://api.risecrowd.org",
-    'invite_url' => "https://www.risecrowd.org"
+    'image_url' => "https://api.alphafund.in",
+    'invite_url' => "https://www.alphafund.in"
 ];
