a2114b49bbe1d5742d4a2f8aabb7db01f8aa6d97
h <aa>
2024-02-11 14:09:48 +0800

    1

--- application/api/controller/Controller.php
+++ application/api/controller/Controller.php
@@ -411,7 +411,7 @@ class Controller extends \think\Controller
         $passwd = '1234567876666666';//加密方法
         $iv  = "1112222211111121";
         $response = openssl_encrypt($data, $method, $passwd, 2,$iv);
-        Log::mylog('response1', $response, 'responsea');
+//        Log::mylog('response1', $response, 'responsea');
         return $response;
         throw new HttpResponseException($response);
     }
