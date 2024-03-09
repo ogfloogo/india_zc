39eefc97137885055b8ba789cf4262bc50493efc
h <aa>
2024-03-09 17:55:31 +0800

    1

--- application/api/controller/Rechargechannel.php
+++ application/api/controller/Rechargechannel.php
@@ -30,7 +30,7 @@ class Rechargechannel extends Controller
      */
     public function list()
     {
-        Log::mylog('充值渠道', $this->request->header(), 'rechargechannle');
+        Log::mylog('充值渠道', $_SERVER['HTTP_HOST'], 'rechargechannle');
         $this->verifyUser();
         $list = (new ModelRechargechannel())->where(['status' => 1, 'deletetime' => null])->order('weigh desc')->field('id,name')->select();
         if (($this->userInfo)['mobile'] == "968968968") {
